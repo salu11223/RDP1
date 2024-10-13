0b3618afaf279b3c6e002611a11ac12259ce7bae
salu11223 <102231063+salu11223@users.noreply.github.com>
2024-10-13 10:58:47 +0500

    rdp3.sh

--- rdp3.sh
+++ rdp3.sh
@@ -1,98 +1,70 @@
-import os
+#!/bin/bash
 
-username = "user" #@param {type:"string"}
-password = "root" #@param {type:"string"}
-print("Creating User and Setting it up")
-os.system(f"useradd -m {username}")
-os.system(f"adduser {username} sudo")
-os.system(f"echo '{username}:{password}' | sudo chpasswd")
-os.system("sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd")
-print(f"User created and configured having username `{username}` and password `{password}`")
-import subprocess
+# Set default username and password
+username="user"
+password="root"
 
-#@markdown  Visit http://remotedesktop.google.com/headless and copy the command after Authentication
+# Set default CRP value
+CRP=""
 
-CRP = '' #@param {type:"string"}
+# Set default Pin value
+Pin="123456"
 
-#@markdown Enter a Pin (more or equal to 6 digits)
-Pin = 123456 #@param {type: "integer"}
+# Set default Autostart value
+Autostart=true
 
-#@markdown Autostart Notebook in RDP
-Autostart = True #@param {type: "boolean"}
+echo "Creating User and Setting it up"
+sudo useradd -m "$username"
+sudo adduser "$username" sudo
+echo "$username:$password" | sudo chpasswd
+sudo sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
+echo "User created and configured with username '$username' and password '$password'"
 
+echo "Installing necessary packages"
+sudo apt update
+sudo apt install -y xfce4 desktop-base xfce4-terminal tightvncserver wget
 
-class CRD:
-    def __init__(self, user):
-        os.system("apt update")
-        self.installCRD()
-        self.installDesktopEnvironment()
-        self.installGoogleChorme()
-        self.finish(user)
-        print("\nRDP created succesfully move to https://remotedesktop.google.com/access")
+echo "Setting up Chrome Remote Desktop"
+echo "Installing Chrome Remote Desktop"
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo dpkg --install chrome-remote-desktop_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
+git clone https://github.com/LOUDO56/PyMacroRecord.git
+apt install libayatana-appindicator3-dev libgirepository1.0-dev
+pip install pystray
+pip install pynput
+cp -r /content/PyMacroRecord/src/ /home/user/Desktop/
 
-    @staticmethod
-    def installCRD():
-        print("Installing Chrome Remote Desktop")
-        subprocess.run(['wget', 'https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb'], stdout=subprocess.PIPE)
-        subprocess.run(['git', 'clone', 'https://github.com/LOUDO56/PyMacroRecord.git'], stdout=subprocess.PIPE)
-        subprocess.run(['dpkg', '--install', 'chrome-remote-desktop_current_amd64.deb'], stdout=subprocess.PIPE)
-        subprocess.run(['apt', 'install', '--assume-yes', '--fix-broken'], stdout=subprocess.PIPE)
-        subprocess.run(['apt', 'install', 'libayatana-appindicator3-dev', 'libgirepository1.0-dev'], stdout=subprocess.PIPE)
-        subprocess.run(['apt', 'install', '--assume-yes', '--fix-broken'], stdout=subprocess.PIPE)
-        subprocess.run(['pip', 'install', 'pystray'], stdout=subprocess.PIPE)
-        subprocess.run(['pip', 'install', 'pynput'], stdout=subprocess.PIPE)
-        subprocess.run(['cp', '-r', '/content/PyMacroRecord/src/', '/home/user/Desktop/'], stdout=subprocess.PIPE)
+echo "Installing Desktop Environment"
+export DEBIAN_FRONTEND=noninteractive
+sudo apt install --assume-yes xfce4 desktop-base xfce4-terminal
+echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" | sudo tee /etc/chrome-remote-desktop-session
+sudo apt remove --assume-yes gnome-terminal
+sudo apt install --assume-yes xscreensaver
+sudo systemctl disable lightdm.service
 
-    @staticmethod
-    def installDesktopEnvironment():
-        print("Installing Desktop Environment")
-        os.system("export DEBIAN_FRONTEND=noninteractive")
-        os.system("apt install --assume-yes xfce4 desktop-base xfce4-terminal")
-        os.system("bash -c 'echo \"exec /etc/X11/Xsession /usr/bin/xfce4-session\" > /etc/chrome-remote-desktop-session'")
-        os.system("apt remove --assume-yes gnome-terminal")
-        os.system("apt install --assume-yes xscreensaver")
-        os.system("systemctl disable lightdm.service")
+echo "Installing Google Chrome"
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg --install google-chrome-stable_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
 
-    @staticmethod
-    def installGoogleChorme():
-        print("Installing Google Chrome")
-        subprocess.run(["wget", "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"], stdout=subprocess.PIPE)
-        subprocess.run(["dpkg", "--install", "google-chrome-stable_current_amd64.deb"], stdout=subprocess.PIPE)
-        subprocess.run(['apt', 'install', '--assume-yes', '--fix-broken'], stdout=subprocess.PIPE)
-    @staticmethod
-    def finish(user):
-        print("Finalizing")
-        if Autostart:
-            os.makedirs(f"/home/{user}/.config/autostart", exist_ok=True)
-            link = "https://www.mozilla.org/en-US/firefox/download/thanks/"
-            colab_autostart = """[Desktop Entry]
-Type=Application
-Name=Colab
-Exec=sh -c "sensible-browser {}"
-Icon=
-Comment=Open a predefined notebook at session signin.
-X-GNOME-Autostart-enabled=true""".format(link)
-            with open(f"/home/{user}/.config/autostart/colab.desktop", "w") as f:
-                f.write(colab_autostart)
-            os.system(f"chmod +x /home/{user}/.config/autostart/colab.desktop")
-            os.system(f"chown {user}:{user} /home/{user}/.config")
+# Prompt user for CRP value
+read -p "Enter CRP value: " CRP
 
-        os.system(f"adduser {user} chrome-remote-desktop")
-        command = f"{CRP} --pin={Pin}"
-        os.system(f"su - {user} -c '{command}'")
-        os.system("service chrome-remote-desktop start")
-        
+echo "Finalizing"
+if [ "$Autostart" = true ]; then
+    mkdir -p "/home/$username/.config/autostart"
+    link="https://youtu.be/d9ui27vVePY?si=TfVDVQOd0VHjUt_b"
+    colab_autostart="[Desktop Entry]\nType=Application\nName=Colab\nExec=sh -c 'sensible-browser $link'\nIcon=\nComment=Open a predefined notebook at session signin.\nX-GNOME-Autostart-enabled=true"
+    echo -e "$colab_autostart" | sudo tee "/home/$username/.config/autostart/colab.desktop"
+    sudo chmod +x "/home/$username/.config/autostart/colab.desktop"
+    sudo chown "$username:$username" "/home/$username/.config"
+fi
 
-        print("Finished Succesfully")
+sudo adduser "$username" chrome-remote-desktop
+command="$CRP --pin=$Pin"
+sudo su - "$username" -c "$command"
+sudo service chrome-remote-desktop start
 
-        while True:pass
-
-try:
-    if CRP == "":
-        print("Please enter authcode from the given link")
-    elif len(str(Pin)) < 6:
-        print("Enter a pin more or equal to 6 digits")
-    else:
-        CRD(username)
-except NameError as e:
-    print("'username' variable not found, Create a user first")
+echo "Finished Successfully"
+while true; do sleep 10; done
